568509b208f47dea7ccc763b2d2b45e76df50d96
Ty Weeks <ty.weeks10@gmail.com>
2022-09-23 21:53:25 -0700

    Added player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -37,6 +37,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMoveKeyboard();
+        AnimatePlayer();
     }
 
     void PlayerMoveKeyboard()
@@ -44,5 +45,23 @@ public class Player : MonoBehaviour
         movementX = Input.GetAxisRaw("Horizontal");
 
         transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer()
+    {
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
     }
 }
