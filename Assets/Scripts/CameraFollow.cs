c49f8ee7157291b48f2964154630081452cdbf5f
Ty Weeks <ty.weeks10@gmail.com>
2022-09-24 19:19:07 -0700

    Improved camera following. Added max and min positions

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,18 +5,25 @@ public class CameraFollow : MonoBehaviour
     private Transform player;
     private Vector3 tempPos;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private float minX, maxX;
+
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         tempPos = transform.position;
         tempPos.x = player.position.x;
         
+        if (tempPos.x < minX)
+            tempPos.x = minX;
+
+        if (tempPos.x > maxX)
+            tempPos.x = maxX;
+
         transform.position = tempPos;
     }
 }
