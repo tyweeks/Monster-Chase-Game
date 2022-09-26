c38d8d7072219013bd2a89322a283ea63f90aa50
Ty Weeks <ty.weeks10@gmail.com>
2022-09-25 21:29:37 -0700

    Instantiating player based on character selection

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
+        int selectedCharacter = int.Parse(EventSystem.current.currentSelectedGameObject.name);
+
+        GameManager.instance.CharIndex = selectedCharacter;
+
         SceneManager.LoadScene("Gameplay");
     }
 }
